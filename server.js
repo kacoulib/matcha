44de15b6978c9cff72b68d50bca3d3d6d8feae9a
karim1412 <coulibaly91karim@gmail.com>
2017-11-19 01:30:22 +0100

    Mlab 'for mongodb' connectin not working

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express	= require('express'),
-		app		= express(),
-		dotenv	= require('dotenv').config();
+const express		= require('express'),
+		app			= express(),
+		mongoClient	= require('mongodb').MongoClient,
+		bodyParser	= require('body-parser'),
+		dotenv		= require('dotenv').config();
 
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoClient.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@ds157833.mlab.com:57833/matcha', (err, db) => 
+{
+	if (err)
+		return (console.log(err));
+	console.log('connected to db')
+})
+
+app.get('/test', (req, res) =>
+{
+	console.log(req.body)
+	console.log(res.body)
+})
+
+app.listen(3000, () => console.log('listening 3000'))
 console.log('ok...');
 console.log(process.env.DB_USER);
