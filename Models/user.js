420a316ad26ea37e53ee9f27eeb8b5e3a5b3c776
karim1412 <coulibaly91karim@gmail.com>
2017-12-01 00:33:01 +0100

    Added npm passport for login, express-session for session management

--- Models/user.js
+++ Models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 let	mongoose		= require('mongoose'),
 	elasticSearch 	= require('elasticsearch'),
@@ -6,124 +5,10 @@ let	mongoose		= require('mongoose'),
 	ObjectId 		= Schema.Types.ObjectId,
 	userSchema,
 	User;
-mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@ds157833.mlab.com:57833/matcha', {
-	  useMongoClient: true,
-	})
-mongoose.set('debug', true); // to remove
-// Schema
-userSchema 	= new Schema(
-{
-	personal :
-	{
-		first_name:
-		{
-			type: String,
-			lowercase: true,
-			trim: true,
-			required: true,
-			validate: (str) => str.indexOf('$') < 0
-		},
-		last_name:
-		{
-			type: String,
-			lowercase: true,
-			trim: true,
-			required: true,
-			validate: (str) => str.indexOf('$') < 0
-		},
-		age:
-		{
-			type : Number,
-			min : 18,
-			max : 110,
-			required: true 
-		},
-		sex:
-		{
-			type : String,
-			enum: ['male', 'female'],
-			required: true
-		},
-		adresses:
-		[{
-			type : String,
-			required : true
-		}],
-		orientation:
-		{
-			type : String,
-			enum: ['heterosexual', 'bisexual', 'homosexual'],
-			required: true
-		},
-		bio: String,
-		email:
-		{
-			type : String,
-			required: true,
-			unique: true,
-			validate: (email) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
-		},
-		pictures:
-		{
-			type :[String],
-			validate : (pics) => pics.length < 5
-		},
-		profil_picture: String,
-	},
-	public : 
-	{
-		tags: [String],
-		viewers: [String],
-		likers: [String]
-	}
-}, { bufferCommands: false });
-
-User		= mongoose.model('User', userSchema);
-
-
-// Api
-exports.create	= (userData) =>
-{
-	let	newUser	= new User(userData);
-
-	 console.log(mongoose);
-	 var promise = newUser.save();
-	 promise.then((err) =>
-	{
-	 	console.log("sending user");
-		if (err)
-			throw err;
-	})
- console.log("after sending user");
-
-}
-
-exports.find	= (search_terms) =>
-{}
-
-exports.update	= (userData) =>
-{
-	let	newUser	= new User(userData);
-
-	newUser.save((err) =>
-	{
-		if (err)
-			throw err;
-	})
-}
-
-exports.delete	= (user_id) =>
-{
-	User.findOne({})
-=======
-'use strict'
-let	mongoose		= require('mongoose'),
-	elasticSearch 	= require('elasticsearch'),
-	Schema			= mongoose.Schema,
-	ObjectId 		= Schema.Types.ObjectId,
-	userSchema,
-	User;
-
+// mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@ds157833.mlab.com:57833/matcha', {
+// 	  useMongoClient: true,
+// 	})
+// mongoose.set('debug', true); // to remove
 // Schema
 userSchema 	= new Schema(
 {
@@ -135,6 +20,7 @@ userSchema 	= new Schema(
 		required: true,
 		validate: (str) => str.indexOf('$') < 0
 	},
+
 	last_name:
 	{
 		type: String,
@@ -143,6 +29,7 @@ userSchema 	= new Schema(
 		required: true,
 		validate: (str) => str.indexOf('$') < 0
 	},
+
 	age:
 	{
 		type : Number,
@@ -150,24 +37,29 @@ userSchema 	= new Schema(
 		max : 110,
 		required: true 
 	},
+
 	sex:
 	{
 		type : String,
 		enum: ['male', 'female'],
 		required: true
 	},
+
 	adresses:
 	[{
 		type : String,
 		required : true
 	}],
+
 	orientation:
 	{
 		type : String,
 		enum: ['heterosexual', 'bisexual', 'homosexual'],
 		required: true
 	},
+
 	bio: String,
+
 	email:
 	{
 		type : String,
@@ -175,16 +67,21 @@ userSchema 	= new Schema(
 		unique: true,
 		validate: (email) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
 	},
+
 	pictures:
 	{
 		type :[String],
 		validate : (pics) => pics.length < 5
 	},
+
 	profil_picture: String,
-	tags: [ObjectId],
+
+	tags: [String],
+
 	viewers: [String],
+
 	likers: [String]
-});
+}, { bufferCommands: false });
 
 User		= mongoose.model('User', userSchema);
 
@@ -194,16 +91,21 @@ exports.create	= (userData) =>
 {
 	let	newUser	= new User(userData);
 
-	// console.log(userData);
+	console.log(userData)
 	newUser.save((err) =>
 	{
+	 	console.log("sending user");
 		if (err)
 			throw err;
 	})
+ console.log("after sending user");
+
 }
 
 exports.find	= (search_terms) =>
-{}
+{
+
+}
 
 exports.update	= (userData) =>
 {
@@ -219,5 +121,4 @@ exports.update	= (userData) =>
 exports.delete	= (user_id) =>
 {
 	User.findOne({})
->>>>>>> dc96621d39dee5c67817c9b34ee3a7d3b080879d
 }
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const express			= require('express'),
+let 	express			= require('express'),
 		app				= express(),
 		admin			= express(),
 		db				= require('./Models/db'),
 		cookieParser	= require('cookie-parser'),
+		session			= require('express-session'),
+		passport		= require('passport'),
 		dotenv			= require('dotenv').config(),
 		bodyParser		= require('body-parser'),
 		auth 			= require('./Middlewares/auth.js');
@@ -14,6 +16,13 @@ db.connect(() =>
 {
 	[app, admin].forEach((route) => 
 	{
+		route.set('trust proxy', 1);
+		route.set(session({
+			secret: '_bibidi_babidi_bou_',
+			name: '42matcha',
+			resave: false,
+  			saveUninitialized: false
+		}));
 		route.use(cookieParser());
 		route.use(bodyParser.urlencoded({extended: true}));
 	})
